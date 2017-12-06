54f1b6a075de081af4de5ab504d23116f7b528cb
Santiago Rodriguez Ortego <santi.r.ortego@gmail.com>
2017-12-06 18:35:49 +0100

    Prueba

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ export default class App extends Component {
         Toast.show({
           text: msg,
           position: 'bottom',
-          buttonText: 'Okay'
+          buttonText: 'Oky'
         })
       },
       this 
